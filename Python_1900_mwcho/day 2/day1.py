f689699823d441e246a52f5832e93f7f046997bd
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-09-15 19:45:07 +0900

    Create day1.py

---
+++ Python_1900_mwcho/day 2/day1.py
@@ -0,0 +1,15 @@
+"""
+    My First Python Program
+    Version : 1.0
+    Created : 2021.09.14
+    Updated : 2021.09.14
+    Author  : J.W.Lee
+"""
+
+# nam 변수에 이름을 넣는다. 파이썬에서 변수 타입 선언은 없다.
+nam = "이모모"
+tim = "19"
+
+# print() 함수를 사용하여 화면에 출력한다.
+print(nam)
+print("내 이름은 " + nam + "이고, " + tim + "시 클래스입니다")
