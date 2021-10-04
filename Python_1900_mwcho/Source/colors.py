71ff77eed2f0a91bd1a70df93538a3ca1a14dd61
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-05 00:12:04 +0900

    Create colors.py

---
+++ Python_1900_mwcho/Source/colors.py
@@ -0,0 +1,17 @@
+CNAME = 'Color Class'
+
+BLACK = '\033[90m'
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+MAGENTA = '\033[95m'
+CYAN = '\033[96m'
+WHITE = '\033[97m'
+
+RED_BG = '\033[101m'
+GREEN_BG = '\033[102m'
+
+END = '\033[0m'
+
+#impor 한 파일명이 너무 길때 별명을 줄 수 있다 import 파일명 as 별명 으로 줌 별명 부여시 원래이름은 사용불가하다
